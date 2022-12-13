8e9d8afcae2c136878b556479fc3a20715ad50c5
kulikovae <forkulikova@gmail.com>
2022-12-13 19:45:16 +0200

    new update

--- update.sh
+++ update.sh
@@ -23,11 +23,3 @@ sudo systemctl daemon-reload
 sudo systemctl enable zeekad
 sudo systemctl restart zeekad
 
-echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ `service zeekad status | grep active` =~ "running" ]]; then
-  echo -e "Your zeekad node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice zeekad status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your zeekad node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
